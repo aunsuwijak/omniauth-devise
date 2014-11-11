2218e6715a2564176651b544b403e0eb89a502b4
Suwijak Chaipipat <suwijakchaipipat@110-238.kuwin.ku.ac.th>
2014-11-11 10:34:10 +0700

    add omniauth-facebook and implement facebook authentication

--- Gemfile
+++ Gemfile
@@ -17,6 +17,10 @@ gem 'coffee-rails', '~> 4.0.0'
 # Devise
 gem 'devise'
 
+# OmniAuth
+gem 'omniauth'
+gem 'omniauth-facebook'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111023019) do
+ActiveRecord::Schema.define(version: 20141111024333) do
 
   create_table "posts", force: true do |t|
     t.datetime "created_at"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20141111023019) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "uid"
+    t.string   "provider"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
